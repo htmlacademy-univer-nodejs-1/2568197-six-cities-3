17b618506433d98306d9e3e92d7eb31ca0818f1e
Nikita Bystritsky <nbystritsky@gmail.com>
2024-12-21 23:44:40 +0500

    feat: 1.19. Приложение для терминала

---
+++ src/shared/types/index.ts
@@ -0,0 +1,7 @@
+export * from './city.enum.js';
+export * from './comment.type.js';
+export * from './rental-amenities.enum.js';
+export * from './rental-offer.type.js';
+export * from './rental-type.enum.js';
+export * from './user-type.enum.js';
+export * from './user.type.js';
---
+++ src/shared/types/user-type.enum.ts
@@ -0,0 +1,4 @@
+export enum UserType {
+  normal = 'normal',
+  pro = 'pro',
+}
