5168d896091fee6b7de804aee4e6328fa4aa3f44
Nikita Bystritsky <nbystritsky@gmail.com>
2025-01-09 16:24:48 +0500

    feat: 6.6. Сервер наносит ответный удар done

--- src/shared/modules/user/user-service.interface.ts
+++ src/shared/modules/user/user-service.interface.ts
@@ -1,6 +1,6 @@
-import { DocumentType } from '@typegoose/typegoose';
-import { UserEntity } from './user.entity.js';
-import { CreateUserDto } from './dto/create-user.dto.js';
+import {CreateUserDto} from './dto/create-user.dto.js';
+import {DocumentType} from '@typegoose/typegoose';
+import {UserEntity} from './user.entity.js';
 
 export interface UserService {
   create(dto: CreateUserDto, salt: string): Promise<DocumentType<UserEntity>>;
